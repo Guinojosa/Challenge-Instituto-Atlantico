df37f223851e513f0f94de8c5926761f6bc96144
Guilherme Nojosa <guilhermenojosa@hotmail.com>
2021-08-24 21:58:53 -0300

    Finish Command Init and Finish first version of command and path with error

--- win-service-listener/Controllers/CommandsController.cs
+++ win-service-listener/Controllers/CommandsController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace win_service_listener.Controllers
 {
@@ -39,17 +41,50 @@ namespace win_service_listener.Controllers
         [HttpGet, Route("GetCommandByPath")]
         public dynamic GetCommandByPath(string path, string command)
         {
-            List<string> response = new List<string>();
-            using (var ps = PowerShell.Create())
+            try
             {
-                var results = ps.AddScript(command).Invoke();
-                foreach (var result in results)
+                string result = string.Empty;
+                using (var ps = PowerShell.Create())
                 {
-                    response.Add(result.ToString());
-                    Console.WriteLine(result.ToString());
-                }
+                    var results = ps.AddScript($"{command} | Out-String").Invoke();   
+                    if(ps.HadErrors) throw new Exception(ps.Streams.Error.ReadAll()[0].ToString());          
+                    if(results.Count == 0) throw new Exception("Command not process.");    
+                    result = results[0].ToString();
+                }      
+                return result;          
             }
-            return response;
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        
+        [HttpGet, Route("Init")]
+        public dynamic Init()
+        {
+            try
+            {
+                string result = string.Empty;
+                using (var ps = PowerShell.Create())
+                {
+                    var resultName = ps.AddScript("hostname  | Out-String").Invoke();     
+                    result += $"Machine Name: \r {resultName[0].ToString()} \n";
+                    var resultIp = ps.AddScript("ipconfig | findstr /i \"ipv4\"  | Out-String").Invoke();     
+                    result += $"Address IPV4: \r\n {resultIp[0].ToString()}\n";
+                    var resultStatusFireWall = ps.AddScript("netsh advfirewall show currentprofile  | Out-String").Invoke();     
+                    result += $"Firewall Settings: \r\n {resultStatusFireWall[0].ToString()}";
+                    var resultAntiVirus = ps.AddScript("Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Out-String").Invoke();     
+                    result += $"AntiVirus Settings: \r\n {resultAntiVirus[0].ToString()}";
+                    var resultDotnetVersion = ps.AddScript("dotnet --version | Out-String").Invoke();     
+                    result += $"Dotnet Version: \r\n {resultDotnetVersion[0].ToString()}";
+                }      
+                return result;          
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
