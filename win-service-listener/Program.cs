1db5ed9ba0148315a626ad4944759c9935d6d88a
Guilherme Nojosa <guilhermenojosa@hotmail.com>
2021-08-27 00:29:44 -0300

    Add NLog

--- win-service-listener/Program.cs
+++ win-service-listener/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.EventLog;
+using NLog.Web;
 
 namespace win_service_listener
 {
@@ -20,10 +21,11 @@ namespace win_service_listener
         public static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
     .ConfigureLogging(logging =>
-        {
-            logging.ClearProviders();
-            logging.AddConsole();
-        })
+      {
+          logging.ClearProviders();
+          logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+      })
+      .UseNLog()
       .ConfigureWebHostDefaults(webBuilder =>
       {
           webBuilder.UseStartup<Startup>();
